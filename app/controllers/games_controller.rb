29ad809b3c9a20a09f416f2b82739d4fcd3b49be
Christopher Breen <C.jbreen61@gmail.com>
2021-02-01 18:43:53 -0500

    updated games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,11 @@
 class GamesController < ApplicationController
+    before_action :find_game, only:[:show, :edit, :destroy, :update]
+    def index
+        @games = Game.all
+    end
+
+    private
+        def find_game
+            @game = Game.find_by_id(params[:id])
+        end
 end
