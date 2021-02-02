69d2fc7d27dbd237ab5db33825728037cdccfc0d
Christopher Breen <C.jbreen61@gmail.com>
2021-02-02 13:21:20 -0500

    seeded db with games

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'rest-client'
+require 'json'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+url = "https://api.rawg.io/api/games"
+resp = RestClient.get(url)
+data = JSON.parse(resp.body)
+
+data["results"].each do |game|
+    Game.create(title: game["name"], release_date: game["released"], description: game["esrb_rating"], image: game["background_image"])
+end
