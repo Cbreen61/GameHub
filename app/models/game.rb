b462f9c263a01b905b80a911c151e766ce1fc966
Christopher Breen <C.jbreen61@gmail.com>
2021-02-03 14:29:41 -0500

    added vaildations to game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
     has_and_belongs_to_many :platforms
     has_many :ratings
-    validates :title, presence: true
+    validates :title, presence: true, uniqueness: true
 end
