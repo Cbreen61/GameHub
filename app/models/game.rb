75b133d18c89a4d29adb5726a8357ee4dffe9726
Christopher Breen <C.jbreen61@gmail.com>
2021-02-04 14:47:49 -0500

    updated rating views

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
     has_and_belongs_to_many :platforms
     has_many :ratings
-    #has_many :user  through: :rating
+    has_many :users,  through: :rating
     validates :title, presence: true, uniqueness: true
 end
