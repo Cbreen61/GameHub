47446f91b6c2707dac73f72638fe58fd71899eee
Christopher Breen <C.jbreen61@gmail.com>
2021-02-04 09:21:56 -0500

    added links for signin,signup and siggnout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
-    skip_before_action :verified_user, only: [:new, :create]
-    
+    #skip_before_action :verified_user, only: [:new, :create]
+
     def signup
         @user = User.new
     end
