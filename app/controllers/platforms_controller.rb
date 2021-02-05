4a64f6d919351c7f306e24d85bb3d9148f5197b1
Christopher Breen <C.jbreen61@gmail.com>
2021-02-04 23:26:37 -0500

    added template ot platforms_controller

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -1,2 +1,34 @@
 class PlatformsController < ApplicationController
+    def index
+
+    end
+
+    def show
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+    
+    def update
+        
+    end
+
+    def destroy
+
+    end
+
+
+
+    private
+ 
 end
+
